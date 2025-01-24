58490e7e456965d192942071cfa43683238e15e5
FrappaF <francesco.pippo.395@gmail.com>
2025-01-24 18:43:18 +0100

    Implemented slicing with dot dot

--- src/include/visitor.h
+++ src/include/visitor.h
@@ -55,6 +55,22 @@ AST_T *visitor_visit_variable(visitor_T *visitor, AST_VARIABLE_T *node);
 AST_T *visitor_visit_variable_with_index(visitor_T *visitor, AST_VARIABLE_T *node, int index);
 
 /**
+ * Visits a variable with max index count node in the AST.
+ * @param visitor The visitor.
+ * @param node The AST node representing the variable count.
+ * @return The result of the visit.
+ */
+AST_T *visitor_visit_last_variable(visitor_T *visitor, AST_VARIABLE_T *node);
+
+/**
+ * Visits a variable with dot dot node in the AST.
+ * @param visitor The visitor.
+ * @param node The AST node representing the variable count.
+ * @return The result of the visit.
+ */
+AST_T *visitor_visit_variable_with_dot_dot(visitor_T *visitor, AST_VARIABLE_T *node, int first_index, int last_index);
+
+/**
  * Visits a variable assignment node in the AST.
  * @param visitor The visitor.
  * @param node The AST node representing the variable assignment.
@@ -190,6 +206,22 @@ AST_T *visitor_visit_term(visitor_T *visitor, AST_T *node);
  * @return The result of the visit.
  */
 AST_T *visitor_visit_factor(visitor_T *visitor, AST_T *node);
+
+/**
+ * Visits a dot dot expresion node in the AST.
+ * @param visitor The visitor.
+ * @param node The AST node representing the nested expression.
+ * @return The result of the visit.
+ */
+AST_T *visitor_visit_dot_dot_expression(visitor_T *visitor, AST_DOT_DOT_EXPRESSION_T *node);
+
+/**
+ * Visits a dot dot annotation node in the AST.
+ * @param visitor The visitor.
+ * @param node The AST node representing the dot dot.
+ * @return The result of the visit.
+ */
+AST_T *visitor_visit_dot_dot(visitor_T *visitor, AST_DOT_DOT_T *node);
 
 /**
  * Visits a function call from a definition node in the AST.
